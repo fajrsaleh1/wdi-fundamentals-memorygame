1074f683ef43b6c918036ab195994d37068fcc01
Fajr Saleh <fajrsaleh@MBPalkhasbFajr.lan>
2019-01-07 15:40:31 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,16 +3,10 @@ console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 // an empty array to hold the cards
 var cardsInPlay = []; 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-// add the first card to cardsInPlay array using Push()
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
 
-//check the user selected two cards
-if(cardsInPlay.length === 2){ 
+var checkForMatch = function(){
+	 //check the user selected two cards
+ if(cardsInPlay.length === 2){ 
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 
@@ -21,5 +15,12 @@ if(cardsInPlay.length === 2){
 
 	}
 }
+ }
 
+var flipCard = function(cardId){	
+
+ console.log("User flipped " + cards[cardId]); 
+ cardsInPlay.push(cards[cardId]);
+ checkForMatch();
+}
 
